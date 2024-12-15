7be01eb87f6bbd60131f564c0429b6f1ad9d9e90
IlgazUygur1 <154981852+IlgazUygur1@users.noreply.github.com>
2024-12-15 23:34:01 +0300

    Initial Commit

---
+++ HellowSwiftui/HellowSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  HellowSwiftuiApp.swift
+//  HellowSwiftui
+//
+//  Created by Ilgaz Uygur on 15.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct HellowSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
